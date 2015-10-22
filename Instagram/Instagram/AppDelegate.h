024d4ce171b4cef11701175ef4e994373b3d3bc8
Greyson Gregory <grey@Greys-MacBook-Air.local>
2015-10-22 15:12:41 -0700

    setup for project - Milestone 1

---
+++ Instagram/Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Greyson Gregory on 10/22/15.
+//  Copyright © 2015 GreyAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # instagram_feed
+
+# https://api.instagram.com/v1/media/popular?client_id=3a3219fc3d0247d3ad7e026562eb53dd
